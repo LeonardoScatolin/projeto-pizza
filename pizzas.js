69532e11f0263fe49a7f4eab88fe5c7d7f00956d
LeonardoScatolin <122055877+LeonardoScatolin@users.noreply.github.com>
2023-02-08 22:45:47 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Deliciosa pizza sabor Mussarela feita com carinho!'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Deliciosa pizza sabor Calabresa feita com carinho!'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Deliciosa pizza sabor Quatro Queijos feita com carinho!'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Deliciosa pizza sabor Americana feita com carinho!'},
+    {id:5, name:'Baiana', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Deliciosa pizza sabor Baiana feita com carinho!'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Deliciosa pizza sabor Moda da Casa feita com carinho!'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Deliciosa pizza sabor Chocolate feita com carinho!'},
+];
